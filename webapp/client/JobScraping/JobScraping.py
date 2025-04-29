cd2b4e96a4f8f88fac7709dc6c2a3641bd99b224
Arfan12630 <arfanrahman464@gmail.com>
2025-04-29 12:17:14 -0400

    Added sessions

--- webapp/client/JobScraping/JobScraping.py
+++ webapp/client/JobScraping/JobScraping.py
@@ -7,6 +7,9 @@ from selenium.webdriver.support import expected_conditions as EC
 from dotenv import load_dotenv
 import os
 import time
+import requests
+from bs4 import BeautifulSoup
+import pickle
 load_dotenv()  # Loads variables from .env
 
 driver_path = os.environ.get("CHROMEDRIVER_PATH")
@@ -14,32 +17,40 @@ print(driver_path)
 
 service = Service(driver_path)
 driver = webdriver.Chrome(service=service)
-driver.get("https://www.linkedin.com/login")
 
+cookies_file = "cookies.pkl"
+jobs_url = "https://www.linkedin.com/jobs"
 
-wait = WebDriverWait(driver, 10)
-email_input = wait.until(EC.presence_of_element_located((By.ID, "username")))
-password_input = driver.find_element(By.ID, "password")
+def login_and_save_cookies():
+    driver.get("https://www.linkedin.com/login")
+    wait = WebDriverWait(driver, 10)
+    email_input = wait.until(EC.presence_of_element_located((By.ID, "username")))
+    password_input = driver.find_element(By.ID, "password")
+    email_input.send_keys(os.environ.get("LINKEDIN_EMAIL"))
+    password_input.send_keys(os.environ.get("LINKEDIN_PASSWORD"))
+    sign_in_button = driver.find_element(By.CLASS_NAME, "btn__primary--large")
+    sign_in_button.click()
+    WebDriverWait(driver, 10).until(EC.url_contains("/feed"))  # Wait for login to complete
+    with open(cookies_file, "wb") as f:
+        pickle.dump(driver.get_cookies(), f)
 
-linkedin_email = os.environ.get("LINKEDIN_EMAIL")
-linkedin_password = os.environ.get("LINKEDIN_PASSWORD")
+def load_cookies_and_navigate():
+    driver.get("https://www.linkedin.com")  # Must visit domain before adding cookies
+    with open(cookies_file, "rb") as f:
+        cookies = pickle.load(f)
+    for cookie in cookies:
+        driver.add_cookie(cookie)
+    driver.get(jobs_url)
 
-email_input.send_keys(linkedin_email)
-password_input.send_keys(linkedin_password)
+if not os.path.exists(cookies_file):
+    login_and_save_cookies()
+    driver.get(jobs_url)
+else:
+    load_cookies_and_navigate()
 
-sign_in_button = driver.find_element(By.CLASS_NAME, "btn__primary--large")
-sign_in_button.click()
+print(driver.page_source)  # Print the HTML after login/cookie load
 
 time.sleep(20)
 driver.quit()
 
 
-
-
-
-
-# # Job Title
-# job_title = "Software Engineer"
-
-# # Location
-# location = "San Francisco, CA"
